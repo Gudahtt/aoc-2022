04c53687618b2ed48df9fceea4cd6566a02d42c8
Mark Stacey <markjstacey@gmail.com>
2022-12-09 01:57:29 -0330

    Add day 2

---
+++ src/days/day2.rs
@@ -0,0 +1,19 @@
+
+pub static EXAMPLE_INPUT: &str = include_str!("../example_input/day2.txt");
+
+fn get_input(custom_input: Option<String>) -> String {
+    return match custom_input {
+        Some(custom_input) => custom_input,
+        None => EXAMPLE_INPUT.to_string(),
+    };
+}
+
+pub fn solve_part_1(custom_input: Option<String>) -> Result<String, String> {
+    let _input = get_input(custom_input);
+    return Err("Not implemented".to_string());
+}
+
+pub fn solve_part_2(custom_input: Option<String>) -> Result<String, String> {
+    let _input = get_input(custom_input);
+    return Err("Not implemented".to_string());
+}
--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,10 @@ fn run_day(day: u8, part: Part, input_path: Option<String>) {
             Part::Part1 => days::day1::solve_part_1(input),
             Part::Part2 => days::day1::solve_part_2(input),
         },
+        2 => match part {
+            Part::Part1 => days::day2::solve_part_1(input),
+            Part::Part2 => days::day2::solve_part_2(input),
+        },
         _ => Err(format!("Day not found: '{}'", day)),
     };
 
